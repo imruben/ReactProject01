cf97af7bf81911a524e1d6e7f20a80920c61cb05
imruben <rubipc21@gmail.com>
2023-01-24 08:52:02 +0100

    states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,37 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpensesDate";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const [date, setDate] = useState(props.date);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
+  const mouseOverHandler = () => {
+    setTitle("EYOOOU");
+  };
+
+  function getRandomDate() {
+    const maxDate = Date.now();
+    const timestamp = Math.floor(Math.random() * maxDate);
+    return new Date(timestamp);
+  }
+  const randomDateHandler = () => {
+    setDate(getRandomDate());
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
+      <button onClick={randomDateHandler}>Random Date</button>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2 onMouseOver={mouseOverHandler}>{title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
