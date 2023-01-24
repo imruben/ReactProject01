80500ede9407744cf292d19951673c4cfa70dcd2
Ruben Peñaranda <rubipc21@gmail.com>
2023-01-24 12:22:36 +0100

    form expenses

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import "./App.css";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  return <Expenses />;
+  return (
+    <div>
+      <NewExpense />
+      <Expenses></Expenses>
+    </div>
+  );
 }
 
 export default App;
